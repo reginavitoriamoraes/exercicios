c6fac187abe12fc404784b7a89d8cd3403e5c87f
ADM_RMORAES4 <ADM_RMORAES4@gerdau.com.br>
2024-03-29 19:31:49 -0300

    adicionado comentarios no exercicio 1

--- exer001/exer001.py
+++ exer001/exer001.py
@@ -1,4 +1,6 @@
+#aqui digita-se as variaveis
 n1= int(input('digite o primeiro valor: '))
 n2= int(input('digite o segundo valor: '))
 s= n1 + n2
 print(f'A soma de {n1} e {n2} é igual a {s}')
+#fim do codigo
