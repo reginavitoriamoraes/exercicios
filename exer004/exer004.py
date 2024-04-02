30d826892e4c2aec16ee73f94f6cb9dce9d08bdb
ADM_RMORAES4 <ADM_RMORAES4@gerdau.com.br>
2024-04-01 21:37:42 -0300

    Novo exercício

---
+++ exer004/exer004.py
@@ -0,0 +1,7 @@
+a = input('Digite oque deseja saber aqui:')
+print('É numero inteiro? {}'.format(a,), a.isnumeric())
+print('É uma letra? {}'.format(a,), a.isalpha())
+print(f'Só tem espaços? {a.isspace()}')
+print(f'O tipo primitivo dele é {type(a)}')
+algo2 = input('Digite o que deseja saber: ')
+print('é um número inteiro? {}' .format(algo2,), a.isnumeric())
---
+++ exer005/exer005.py
@@ -0,0 +1,50 @@
+from decimal import Decimal
+
+n1=int (input('Digite um numero:'))
+suc= n1+1 
+ant= n1-1 
+print(f'O sucessor de n1 é {suc} e o antecessor é {ant}')
+
+n2=int (input('Digite um valor: '))
+sucessor= n2+1
+antecessor=n2-1
+print(f'O sucessor de n2 é {sucessor} e o antecessor é {antecessor}')
+
+n3=int (input('Digite outro valor: '))
+sucessor=n2+1
+antecessor= n2-1
+print(f'O sucessor de n3 é {sucessor} e o antecessor é {antecessor}')
+
+n4=int (input('Digite um valor: '))
+sucessor=n4+1
+antecessor=n4-1
+print(f'o sucessor de n4 é {sucessor} e o antecessor é {antecessor}')
+
+n5=Decimal (input('Digite outro valor: '))
+sucessor=n5+1
+antecessor=n5-1
+print(f'o sucessor de n5 é {sucessor} e o antecessor é {antecessor}')
+
+
+n6=Decimal (input('Digite outro valor: '))
+sucessor=n6+1
+antecessor=n6+1
+print(f'O sucessor é {sucessor} e o antecessor é {antecessor}')
+
+
+
+n7=Decimal (input('Digite outro valor:'))
+sucessor=n7+1
+antecessor=n7-1
+print(f'O sucessor é {sucessor} e o antecessor é {antecessor}')
+
+
+n8=Decimal (input('Digite outro valor: '))
+sucessor=n8+1
+antecessor=n8-1
+print(f'o sucessor é {sucessor} e o antecessor é {antecessor}')
+            
+n9=Decimal (input('Digite outro valor: '))
+sucessor= n9+1
+antecessor=n9-1
+print(f'O sucessor é {sucessor} e o antecessor é {antecessor}')
